11dff2ad533ed1e702b648228edf324d0052e386
mammu26 <64007748+mammu26@users.noreply.github.com>
2020-05-04 18:35:25 +0530

    env changes

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -5,10 +5,10 @@ export IMAGE_NAME=netcoreapps-todoworld
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
-export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=todoworld.netcore.io
+export AWS_ECS_CLUSTER_NAME=App-cluster
+export AWS_VIRTUAL_HOST=ec2-35-153-232-50.compute-1.amazonaws.com
 export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
-export LETSENCRYPT_EMAIL=team@servicestack.net
+export LETSENCRYPT_EMAIL=mamtha26@gmail.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
