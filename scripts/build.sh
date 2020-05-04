f08c3212542c6d492ca13237100ca83f06e9784c
mammu26 <64007748+mammu26@users.noreply.github.com>
2020-05-04 23:09:23 +0530

    docker command fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source ../deploy-envs.sh
+source deploy-envs.sh
 
 #AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
+docker run -v /var/run/docker.sock:/var/run/docker.sock
 docker build -t $IMAGE_NAME ../
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
