cb07924ccf564d68eb1a3ea88b1d432567aff13a
mammu26 <64007748+mammu26@users.noreply.github.com>
2020-05-04 18:52:12 +0530

    changes

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -9,6 +9,7 @@ export AWS_ECS_CLUSTER_NAME=App-cluster
 export AWS_VIRTUAL_HOST=ec2-35-153-232-50.compute-1.amazonaws.com
 export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
 export LETSENCRYPT_EMAIL=mamtha26@gmail.com
+export AWS_ACCOUNT_NUMBER=358342348051
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
