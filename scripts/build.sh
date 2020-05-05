741bb329b0fc10403beee68f4abdfd4c78bfb450
mammu26 <64007748+mammu26@users.noreply.github.com>
2020-05-05 08:27:31 +0530

    added sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ source deploy-envs.sh
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
-docker run -v /var/run/docker.sock:/var/run/docker.sock ...
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+#docker run -v /var/run/docker.sock:/var/run/docker.sock ...
+sudo docker build -t $IMAGE_NAME .
+sudo docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
